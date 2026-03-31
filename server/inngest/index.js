86fc0c5ddb967c4cf63cff3efd7b033ab9864ac3
Aarsh Singh <starfire9599@gmail.com>
2026-03-31 12:04:31 +0530

    Bug fix in inngest function arguments

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -6,9 +6,11 @@ export const inngest = new Inngest({ id: "pingup-app" });
 
 // Inngest function to save user data to a database
 const syncUserCreation = inngest.createFunction(
-  { id: "sync-user-from-clerk" },
-  { event: "clerk/user.created" },
-  async (event) => {
+  {
+    id: "sync-user-from-clerk",
+    triggers: [{ event: "clerk/user.created" }],
+  },
+  async ({event}) => {
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
     let username = email_addresses[0].email_address.split("@")[0];
@@ -34,8 +36,10 @@ const syncUserCreation = inngest.createFunction(
 
 // Inngest function to update user data to a database
 const syncUserUpdation = inngest.createFunction(
-  { id: "update-user-from-clerk" },
-  { event: "clerk/user.updated" },
+  {
+    id: "update-user-from-clerk",
+    triggers: [{ event: "clerk/user.updated" }],
+  },
   async (event) => {
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
@@ -52,8 +56,10 @@ const syncUserUpdation = inngest.createFunction(
 
 // Inngest function to delete user from database
 const syncUserDeletion = inngest.createFunction(
-  { id: "delete-user-from-clerk" },
-  { event: "clerk/user.deleted" },
+  {
+    id: "delete-user-from-clerk",
+    triggers: [{ event: "clerk/user.deleted" }],
+  },
   async (event) => {
     const { id } = event.data;
 
