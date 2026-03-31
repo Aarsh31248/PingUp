5440b62a67bb2a7758a29d02479adfe12f5510e2
Aarsh Singh <starfire9599@gmail.com>
2026-03-31 19:52:30 +0530

    feat: API: Update user data

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,7 @@
+import { format } from "path";
+import imagekit from "../configs/imageKit.js";
 import User from "../models/User.js";
+import fs from "fs";
 
 // Get user data using userId
 export const getUserData = async (req, res) => {
@@ -15,4 +18,77 @@ export const getUserData = async (req, res) => {
   }
 };
 
-// 
+// Update user data
+export const updateUserData = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { username, bio, location, full_name } = req.body;
+
+    const tempUser = await User.findById(userId);
+
+    !username && (username = tempUser.username);
+    if (tempUser.username !== username) {
+      const user = await User.findOne({ username });
+      if (user) {
+        // we will not chnage the username id it is already taken
+        username = tempUser.username;
+      }
+    }
+
+    const updatedData = {
+      username,
+      bio,
+      location,
+      full_name,
+    };
+
+    const profile = req.files.profile && req.files.profile[0];
+    const cover = req.files.cover && req.files.cover[0];
+
+    if (profile) {
+      const buffer = fs.readFileSync(profile.path);
+      const response = await imagekit.upload({
+        file: buffer,
+        fileName: profile.originalname,
+      });
+
+      const url = imagekit.url({
+        path: response.filePath,
+        transformation: [
+          { quality: "auto" },
+          { format: "webp" },
+          { width: "512" },
+        ],
+      });
+
+      updatedData.profile_picture = url;
+    }
+
+    if (cover) {
+      const buffer = fs.readFileSync(cover.path);
+      const response = await imagekit.upload({
+        file: buffer,
+        fileName: cover.originalname,
+      });
+
+      const url = imagekit.url({
+        path: response.filePath,
+        transformation: [
+          { quality: "auto" },
+          { format: "webp" },
+          { width: "1280" },
+        ],
+      });
+
+      updatedData.cover_photo = url;
+    }
+
+    const user = await User.findByIdAndUpdate(userId, updatedData, {new: true})
+
+    res.json({success: true, user, message: "Profile Updated Successfully"})
+
+  } catch (error) {
+    console.log(error);
+    return res.json({ success: false, message: error.message });
+  }
+};
