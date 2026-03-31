603d507d5c43fe4a3e24d4ef0ddfb5975349a523
Aarsh Singh <starfire9599@gmail.com>
2026-03-31 18:26:12 +0530

    feat: API: Get user data using userId

---
+++ server/controllers/userController.js
@@ -0,0 +1,18 @@
+import User from "../models/User.js";
+
+// Get user data using userId
+export const getUserData = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.json({ success: false, message: "User not found" });
+    }
+    res.json({ success: true, user });
+  } catch (error) {
+    console.log(error);
+    return res.json({ success: false, message: error.message });
+  }
+};
+
+// 
